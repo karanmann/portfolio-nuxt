8b56b64ffce1c2c71fe7ecfa053d08bc117a6f5e
Karan Mann <karan.mann@outlook.com>
2023-02-22 21:47:24 +0100

    dev: text adjusted

--- client/components/AboutMe.vue
+++ client/components/AboutMe.vue
@@ -92,7 +92,7 @@ export default {
     flex-wrap: wrap;
     justify-content: flex-start;
     .about-me__bottom-container-right-pills-font {
-      font-size: 12px;
+      font-size: 14px;
       font-weight: 400;
       @media (min-width: $desktop) {
         font-size: 16px;
