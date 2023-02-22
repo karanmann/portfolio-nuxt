5c47648c40a183e3d7186695bff10110b6163f93
Karan Mann <karan.mann@outlook.com>
2023-02-22 22:04:39 +0100

    dev: font reduced

--- client/components/AboutMe.vue
+++ client/components/AboutMe.vue
@@ -98,7 +98,7 @@ export default {
       width: 100% auto;
       color: black;
       border-radius: 7px;
-      font-size: 14px;
+      font-size: 12px;
       font-weight: 400;
       @media (min-width: $desktop) {
         font-size: 16px;
